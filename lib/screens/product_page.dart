69ca39ce71d92a8d06af8232f8da0dc54b7964a6
Tanmay <kumartanmay315@gmail.com>
2021-01-15 20:27:30 +0530

    Home Tab

---
+++ lib/screens/product_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ProductPage extends StatefulWidget {
+  final String productId;
+  ProductPage({this.productId});
+
+  @override
+  _ProductPageState createState() => _ProductPageState();
+}
+
+class _ProductPageState extends State<ProductPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
